341e98f253c88c007e9a097aa089bb711dd01eca
Vincent Hillenbrink <vincent@vincent.net>
2010-08-04 18:58:03 +0200

    Added per-setting virtual hosts to create_apache_vhost_conf.sh

--- scripts/create_apache_vhost_conf.sh
+++ scripts/create_apache_vhost_conf.sh
@@ -19,21 +19,51 @@ domain=local
 
 cd `dirname $0`
 
+echo '#' Generated virtual host config
+echo
+
 for django_project_dir in $PROJECT_ROOT/*; do
+
+    # Generic settings
     if [ -f "$django_project_dir/settings.py" -o \
          -d "$django_project_dir/settings" ]; then
         django_project=`basename $django_project_dir`
-        export PYTHONPATH=$django_project_dir
-        export DJANGO_SETTINGS_MODULE=settings
+
+        echo '#' $django_project
+        export PYTHONPATH=`dirname $django_project_dir`
+        export DJANGO_SETTINGS_MODULE=$django_project.settings
         port=`get_django_setting LOCAL_SERVER_PORT`
 
-cat << EOF
+        cat << EOF
 <VirtualHost 127.0.0.1:*>
     ServerName $django_project.$domain
     RewriteEngine On
     RewriteRule ^/(.*) http://localhost:$port/\$1 [P]
 </VirtualHost>
 EOF
-        
+
+        # Environment settings
+        for settings in $django_project_dir/settings/env/[a-z]*py; do
+            django_project_dir=`echo $settings | sed "s#/settings/env/.*py##"`
+            django_project=`basename $django_project_dir`
+            django_settings=`echo $settings | sed "s#$PROJECT_ROOT/##" | sed "s#[a-z]*/settings/env/#settings.env.#" | sed 's#.py$##'`
+            django_settings_id=`echo $django_settings | sed "s#.*\\.##"`
+
+            echo '#' $django_project $django_settings
+
+            export PYTHONPATH=`dirname $django_project_dir`
+            export DJANGO_SETTINGS_MODULE=$django_project.$django_settings
+            port=`get_django_setting LOCAL_SERVER_PORT`
+
+            cat << EOF
+<VirtualHost 127.0.0.1:*>
+    ServerName $django_settings_id.$django_project.$domain
+    RewriteEngine On
+    RewriteRule ^/(.*) http://localhost:$port/\$1 [P]
+</VirtualHost>
+EOF
+        done
+
+        echo
     fi
 done
