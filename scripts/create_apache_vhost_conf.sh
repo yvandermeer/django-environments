767c544c9440609db015a674a19b57208e5b5e74
Yuri van der Meer <info@yvandermeer.net>
2010-10-21 15:27:21 +0200

    Fixed bug in create_apache_vhost_conf.sh when numbers occur in settings modules

--- scripts/create_apache_vhost_conf.sh
+++ scripts/create_apache_vhost_conf.sh
@@ -90,14 +90,14 @@ for django_project_dir in $PROJECT_ROOT/*; do
 EOF
 
         # Environment settings
-        for settings in $django_project_dir/settings/env/[a-z_]*py; do
+        for settings in $django_project_dir/settings/env/[a-z0-9_]*py; do
             if [ `basename $settings` = "__init__.py" -o ! -f "$settings" ]; then
                 continue
             fi
 
             django_project_dir=`echo $settings | sed "s#/settings/env/.*py##"`
             django_project=`basename $django_project_dir`
-            django_settings=`echo $settings | sed "s#$PROJECT_ROOT/##" | sed "s#[a-z_]*/settings/env/#settings.env.#" | sed 's#.py$##'`
+            django_settings=`echo $settings | sed "s#$PROJECT_ROOT/##" | sed "s#[a-z0-9_]*/settings/env/#settings.env.#" | sed 's#.py$##'`
             django_settings_id=`echo $django_settings | sed "s#.*\\.##"`
 
             echo '#' $django_project $django_settings
