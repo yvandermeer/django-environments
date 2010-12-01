f3f1c8b5a6c2b50fb3024b871768e3b9b7ab331e
Vincent Hillenbrink <vincent@vincent.net>
2010-12-01 18:33:14 +0100

    Deal better with $PROJECT_ROOT having no Django projects; code cleanup; more messages to stderr

--- bin/run_in_env_wrapper.sh
+++ bin/run_in_env_wrapper.sh
@@ -18,11 +18,11 @@ PYTHONPATH=$PROJECT_ROOT/lib:$PYTHONPATH
 
 # Check for help request
 if [ "$1" = "-h" -o "$1" = "--help" ]; then
-    echo "Usage: `basename $0` <command> [args...]"
-    echo
-    echo "Where <command> is any of the following:"
+    echo "Usage: `basename $0` <command> [args...]" 1>&2
+    echo 1>&2
+    echo "Where <command> is any of the following:" 1>&2
     for file in $APP_HOME/*; do
-        [ -x $file -a `basename $file` != `basename $0` ] && echo "    "`basename $file`
+        [ -x $file -a `basename $file` != `basename $0` ] && echo "    "`basename $file` 1>&2
     done
     exit
 fi
