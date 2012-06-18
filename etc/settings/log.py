0f7eafcd9e1d27d08b22c065a842a37bbe1fa972
Yuri van der Meer <info@yvandermeer.net>
2012-06-18 15:31:08 +0200

    Django 1.4 compatible etc.settings.log

--- etc/settings/log.py
+++ etc/settings/log.py
@@ -6,10 +6,54 @@ from .generic import PROJECT_ROOT, PROJECT
 
 LOGGING_FILENAME = path.join(PROJECT_ROOT, 'log/%s.log' % PROJECT)
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s - %(name)-30s - %(levelname)-8s: %(message)s',
-    #format='%(name)s: %(message)s',
-    #datefmt='%a, %d %b %Y %H:%M:%S',
-    filename=LOGGING_FILENAME,
-)
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s - %(name)-30s - %(levelname)-8s: %(message)s',
+        },
+        'devserver': {
+            'format': '[%(asctime)s] %(name)s: %(message)s',
+            'datefmt': '%d/%b/%Y %H:%M:%S',
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler',
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'devserver',
+        },
+        'file':{
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'formatter': 'verbose',
+            'filename': LOGGING_FILENAME,
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file'],
+            'propagate': True,
+            'level': 'WARNING',
+        },
+        'django': {
+            'handlers': ['null'],
+            'propagate': False,
+            'level': 'INFO',
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    }
+}
