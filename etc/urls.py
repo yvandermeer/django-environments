f27e4bf1dbc7c5bf7d897777fd61248c0cece8c5
Vincent Hillenbrink <vincent@vincent.net>
2012-07-27 17:08:59 +0200

    Cleaner etc.url with i18n

--- etc/urls.py
+++ etc/urls.py
@@ -1,16 +1,18 @@
-from django.conf.urls.defaults import patterns, include, url, handler404, handler500
-handler404, handler500 # Keep pyflakes happy...
+from django.conf.urls.defaults import url, include, patterns
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.contrib import databrowse
 from django.conf import settings
 
 
-__all__ = ('default_patterns', 'patterns', 'include', 'url', 'handler404', 'handler500')
+__all__ = ('default_patterns', 'default_i18n_patterns')
 
 admin.autodiscover()
 
-default_patterns = patterns('',
+_patterns = (
     url(r'^admin/', include(admin.site.urls)),
     url(r'^%s$' % settings.LOGIN_URL[1:], 'django.contrib.auth.views.login',
          {'template_name': 'admin/login.html'})
 )
+
+default_patterns = patterns('', *_patterns)
+default_i18n_patterns = i18n_patterns('', *_patterns)
